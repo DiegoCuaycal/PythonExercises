70904e625cb8d6bc109dc567bfe54108f3aa250f
DiegoCuaycal <diegocuaycal01@gmail.com>
2023-04-18 20:05:40 -0500

    Realize un coomit

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -5,3 +5,5 @@ pCorrecto =  (100/total)*correctos
 pErores = (100/total)*errores
 print(" Su puntaje final fue : "+str(correctos)+"/"+str(total))
 print(" Su porcentaje de acierto es : %.2f "%(pCorrecto,pErores))
+
+print("Excelente")
